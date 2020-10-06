f7d2914c97e23bb3a5d3637d524e375fa824d541
drr00t <adribeiro@gmail.com>
2020-10-06 19:41:41 -0300

    few more fix codacy standards :)

--- Complexity_and_Scope/TodoAgility.Agile/Domain/BusinessObjects/Description.cs
+++ Complexity_and_Scope/TodoAgility.Agile/Domain/BusinessObjects/Description.cs
@@ -52,8 +52,16 @@ namespace TodoAgility.Agile.Domain.BusinessObjects
 
         public bool Equals(Description other)
         {
-            if (ReferenceEquals(null, other)){ return false;}
-            if (ReferenceEquals(this, other)){ return true;}
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
             return string.Equals(_description, other._description);
         }
 
@@ -64,9 +72,21 @@ namespace TodoAgility.Agile.Domain.BusinessObjects
 
         public override bool Equals(object obj)
         {
-            if (ReferenceEquals(null, obj)){ return false;}
-            if (ReferenceEquals(this, obj)){ return true;}
-            if (obj.GetType() != this.GetType()){ return false;}
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
             return Equals((Description) obj);
         }
 
